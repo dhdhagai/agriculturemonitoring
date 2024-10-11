31bb6f1a08fdb4889a7ec0b336f376207259ca3e
dhdhagai <84077442+dhdhagai@users.noreply.github.com>
2024-10-11 13:38:49 +0530

    Merged Designdev to main
    * Major Changes Done to Home Page and Header Part
    
    * Added "liveview.html" and made some changes to "header.html"
    
    * Major Changes Made to the files
    
    * minor changes
    
    * Fixed Some design issues. Broken Login Page Made
    
    * Updated all issues and add items according to the issues opened by the team
    
    * Fixed missing components of the website
    
    ---------
    
    Co-authored-by: saishinkar2 <141svs@gmail.com>
    Co-authored-by: saishinkar2 <116909682+saishinkar2@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+import secretPhrase from "./env.js"
+
 document.getElementById('chat-form').addEventListener('submit', async (e) => {
-    console.log("Test")
     e.preventDefault();
+    const apiKey = secretPhrase();
+    console.log("Test")
+
     const input = document.getElementById('user-input');
     const message = input.value;
     if (!message) return;
@@ -8,16 +12,31 @@ document.getElementById('chat-form').addEventListener('submit', async (e) => {
     addMessage('user', message);
     input.value = '';
 
-    const response = await fetch('/api/chat', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({message}),
-    });
-
-    const data = await response.json();
-    addMessage('bot', data.reply);
+    const userMessage = input;
+    console.log(userMessage);
+    
+    try {
+const data = {"contents":[{"parts":[{"text":message}]}]}
+        
+        fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            addMessage("BOT: ",data.candidates[0].content.parts[0].text)
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    
+    } catch (error) {
+        console.error('Error making API request:', error);
+    }
+    
 });
 
 function addMessage(sender, message) {
@@ -26,12 +45,8 @@ function addMessage(sender, message) {
     var converter = new showdown.Converter();
     var md = message;
     var html = converter.makeHtml(md);
-
-    // Add sender and message to the div
     div.classList.add('chat-message');
     div.innerHTML = `<strong>${sender}:</strong> ${html}`;
-
-    // Append the message to the messages container
     messages.appendChild(div);
     messages.scrollTop = messages.scrollHeight;
 }
